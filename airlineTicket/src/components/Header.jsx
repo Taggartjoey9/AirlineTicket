2805aaf30119385a50aba96eadc58ddb7a06b0f9
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-15 11:00:46 -0500

    setting up header nav bar/fonts

---
+++ airlineTicket/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav-container">
+                <h1>Airline Tickets</h1>
+                <ul className="nav-items">
+                    <li>Delta</li>
+                    <li>American Airlines</li>
+                    <li>United Airlines</li>
+                </ul>
+            </nav>
+        </header>
+)}
