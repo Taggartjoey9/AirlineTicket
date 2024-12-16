e875ce8431a0ec49b6d848e6a8b91c1f68596260
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-15 19:41:29 -0500

    setting up header nav and images

--- airlineTicket/src/components/Header.jsx
+++ airlineTicket/src/components/Header.jsx
@@ -1,13 +1,27 @@
 export default function Header() {
     return (
-        <header>
-            <nav className="nav-container">
+        <header className="header-nav">
+            <div className="header-left">
                 <h1>Airline Tickets</h1>
-                <ul className="nav-items">
-                    <li>Delta</li>
+                <img src="./main-logo.png" alt="airplane image" />
+            </div>
+              <ul className="nav-items">
+                <div className="delta">
+                    <img src="Delta.png" alt="" />
+                    <li>Delta Airlines</li>
+                </div>
+                <div className="american">
+                    <img src="AA.png" alt="" />
                     <li>American Airlines</li>
+                </div>
+                <div className="united">
+                    <img src="United.png" alt="" />
                     <li>United Airlines</li>
-                </ul>
-            </nav>
+                </div>
+                <div className="southwest">
+                    <img src="SW.png" alt="" />
+                    <li>Southwest Airlines</li>
+                </div>
+              </ul>
         </header>
 )}
