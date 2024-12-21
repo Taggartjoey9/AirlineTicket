f3aa7bd3046a46775eb9c3b0adee3627a9b79d71
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-20 20:42:55 -0500

    destination card styling updates

--- airlineTicket/src/components/DestinationCards.jsx
+++ airlineTicket/src/components/DestinationCards.jsx
@@ -4,7 +4,13 @@ export default function DestinationCards({ headerText }) {
         <>
         <h1 className="card-header-text">{headerText}</h1>
         <container className="main-card-container" >
-            <div className="card-container"></div>
+            <div className="card-container">
+                <img src="sanDiego.jpg" alt="golden gate bridge" />
+                <h3>Charlotte, NC (CLT)<br></br>to<br></br>San Diego, CA (SAN)</h3>
+                <h5>02/05/25 - 02/12/25</h5>
+                <h4>from $359* Updated: 7 hours ago Round trip | Economy</h4>
+                <button>See Latest Fare</button>
+            </div>
             <div className="card-container"></div>
             <div className="card-container"></div>
             <div className="card-container"></div>
