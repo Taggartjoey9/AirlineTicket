f058af1ec4e20c9f40fc2f450d88d708614469c6
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-27 20:34:33 -0500

    updated hero images with final styling

--- airlineTicket/src/components/Hero.jsx
+++ airlineTicket/src/components/Hero.jsx
@@ -3,7 +3,14 @@
 export default function Hero() {
     return (
         <section className="hero-container">
-            <p className="hero-text"></p>
+            <div className="hero-text-container">
+                <div className="text-inner-content">
+                    <h1>Legacy Airline Tickets</h1>
+                    <h4>At a click of a button &raquo;</h4>
+                    <p>At Aero Stub, we strive to provide the best selection of travel needs regarding Legacy Airlines. We allow you to check weather for your destination, browse the most visited locations, and get the best up to date pricing for tickets so your dollar goes further at Aero Stub!</p>
+                </div>
+            </div>
+            
             <div className="hero-image-container">
                 <img className="hero-image" src="./arrivals-sign.png" alt="airport arrival sign" />
             </div>
